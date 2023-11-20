a54781f1201d07365db932858fabdc5d8b5e6333
zavx0z <zavx0z@yahoo.com>
2023-11-20 14:22:25 +0300

    devtools

---
+++ dev/devTools.js
@@ -0,0 +1,16 @@
+eruda.init({
+  default: {
+    displaySize: 40,
+    transparency: 1,
+    theme: "Material Oceanic",
+  },
+})
+if (localStorage.getItem("eruda-active") === "true") eruda.show()
+eruda._entryBtn._$el[0].addEventListener("click", (event) => {
+  setTimeout(() => {
+    localStorage.setItem(
+      "eruda-active",
+      eruda._$el[0].children[0].style.display === "block" ? "true" : "false"
+    )
+  }, 300)
+})
