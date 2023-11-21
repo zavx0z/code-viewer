8ea59b991facdef8cffd573a49286d0c1f4376c2
zavx0z <metaversebdfl@gmail.com>
2023-11-22 02:56:46 +0300

    optimize

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import worker from "./worker.js"
 
-const template = document.createElement("template")
-template.innerHTML = `
-    <link rel="stylesheet" type="text/css" href="./styles.css" />
-    <pre class="line-num"><code></code></pre>
+const template = Object.assign(document.createElement("template"), {
+  innerHTML: /*html*/ `
+ <link rel="stylesheet" type="text/css" href="./styles.css" />
+ <pre class="line-num"><code></code></pre>
 `
+})
+
 class CodeViewer extends HTMLElement {
   name = "Подсветка синтаксиса кода"
   input = {
@@ -19,13 +21,12 @@ class CodeViewer extends HTMLElement {
 
   constructor() {
     super()
-    this.root = this.attachShadow({ mode: "closed" })
-    let clone = template.content.cloneNode(true)
-    this._preview = clone.querySelector("code")
-    this.root.appendChild(clone)
+    const root = this.attachShadow({ mode: "closed" })
+    this._preview = template.content.querySelector("code")
+    root.appendChild(template.content)
   }
   connectedCallback() { }
-  attributeChangedCallback(attrName, oldValue, newValue) { 
+  attributeChangedCallback(attrName, oldValue, newValue) {
     console.log(attrName)
   }
   /**
@@ -37,12 +38,7 @@ class CodeViewer extends HTMLElement {
     this.property = property
     this._preview.innerHTML = output.dst.value
   }
-  send({
-    fold = this.input.fold.default,
-    lineno = this.input.lineno.default,
-    src = this.input.src.default,
-    lang = "js"
-  }) {
+  send({ fold = this.input.fold.default, lineno = this.input.lineno.default, src = this.input.src.default, lang = "js" }) {
     worker(fold, lineno, src, lang).then((result) => {
       this._preview.innerHTML = result
       this.input.fold.value = fold
