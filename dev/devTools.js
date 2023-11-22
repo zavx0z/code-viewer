42cd14afb4f55747821a43b54e5021a612d1a674
zavx0z <metaversebdfl@gmail.com>
2023-11-22 14:02:14 +0300

    devtools is mobile

--- dev/devTools.js
+++ dev/devTools.js
@@ -1,16 +1,21 @@
-eruda.init({
-  default: {
-    displaySize: 40,
-    transparency: 1,
-    theme: "Material Oceanic",
-  },
-})
-if (localStorage.getItem("eruda-active") === "true") eruda.show()
-eruda._entryBtn._$el[0].addEventListener("click", (event) => {
-  setTimeout(() => {
-    localStorage.setItem(
-      "eruda-active",
-      eruda._$el[0].children[0].style.display === "block" ? "true" : "false"
-    )
-  }, 300)
-})
+if (navigator.maxTouchPoints && navigator.maxTouchPoints > 2) {
+  await import("//cdn.jsdelivr.net/npm/eruda")
+  eruda.init({
+    default: {
+      displaySize: 40,
+      transparency: 1,
+      theme: "Material Oceanic",
+    },
+  })
+  if (localStorage.getItem("eruda-active") === "true") eruda.show()
+  eruda._entryBtn._$el[0].addEventListener("click", (event) => {
+    setTimeout(() => {
+      localStorage.setItem(
+        "eruda-active",
+        eruda._$el[0].children[0].style.display === "block" ? "true" : "false"
+      )
+    }, 300)
+  })
+}
+
+
